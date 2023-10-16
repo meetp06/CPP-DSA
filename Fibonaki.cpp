0b0a661a2ab069c0ed4be87703d2b4a56165ec2f
Meet0006 <meetp0006@gmail.com>
2023-10-17 03:07:01 +0530

    Fibonaki

---
+++ Fibonaki.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+// For Loop
+int main() {
+    int n;
+    cout << "Enter number: "; // Enter above 30 num; For better results.
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+
+        cout << i << " ";
+    }
+}
