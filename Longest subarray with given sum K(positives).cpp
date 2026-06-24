18eb974c9d8d63ae555be5fb5beff308c9d2c94f
Meetpatel06 <65815199+meetp06@users.noreply.github.com>
2026-06-24 13:53:37 -0700

    Implement second solution for longest subarray sum K
    Added a second solution to find the longest subarray with a given sum K using a sliding window approach.

--- Longest subarray with given sum K(positives).cpp
+++ Longest subarray with given sum K(positives).cpp
@@ -1,3 +1,4 @@
+// Solution 1
 #include<iostream>
 #include<vector>
 #include<map>
@@ -28,5 +29,39 @@ for(int i=0; i< arr.size();i++){
     }
     cout<<stoindex;
     }
+
+// Solution 2
+
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
+int main(){
+    
+vector<int> arr = {2,0,0,1,1,1,1,1,4,2,3}; // 5
+int target = 3;
+map<int, int> mp;
+int left=0,right=0;
+int store = 0;
+int stlen = 0;
+
+while(right < arr.size() ){
+    store += arr[right];
+    
+    while(target < store && left <= right){
+        store -= arr[left];
+        left++;
+        }
+        
+        if(store == target ){
+            stlen = max(stlen, right -left +1);
+            }
+            right++;
+    }
+cout<<stlen;
+
+}
+    
     
     
